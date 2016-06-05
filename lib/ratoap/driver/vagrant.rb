1298672fc4c88c176d57837dfe3a58744280a13d
vkill <vkill.net@gmail.com>
2016-06-05 20:10:30 +0800

    add logger and redis

--- lib/ratoap/driver/vagrant.rb
+++ lib/ratoap/driver/vagrant.rb
@@ -1,9 +1,30 @@
 require_relative "vagrant/version"
 
+require "logger"
+require "redis"
+
 module Ratoap
   module Driver
     module Vagrant
-      # Your code goes here...
+
+      def self.logger
+        @@logger ||= (
+          Logger.new(STDOUT)
+        )
+      end
+
+      def self.redis
+        @@redis ||= (
+          redis_config = {
+            host: '127.0.0.1',
+            port: 6379,
+            db: 0,
+          }
+
+          Redis.new redis_config
+        )
+      end
+
     end
   end
 end
--- lib/ratoap/driver/vagrant/cli.rb
+++ lib/ratoap/driver/vagrant/cli.rb
@@ -1,9 +1,11 @@
 module Ratoap
-  module CLI
-
-    def self.run(argv = ARGV)
-      0
+  module Driver
+    module Vagrant
+      module CLI
+        def self.run(argv = ARGV)
+          0
+        end
+      end
     end
-
   end
 end
