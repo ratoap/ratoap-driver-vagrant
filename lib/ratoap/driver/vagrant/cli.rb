08fdc80aba01d5a5a4500f5b3eb44f12c8599ec0
vkill <vkill.net@gmail.com>
2016-06-06 10:05:38 +0800

    add redis subscribe

--- lib/ratoap/driver/vagrant/cli.rb
+++ lib/ratoap/driver/vagrant/cli.rb
@@ -3,6 +3,16 @@ module Ratoap
     module Vagrant
       module CLI
         def self.run(argv = ARGV)
+          logger = Ratoap::Driver::Vagrant.logger
+          redis = Ratoap::Driver::Vagrant.redis
+
+          logger.info "subscribe ratoap:client_conn"
+          redis.subscribe_with_timeout(5, "ratoap:client_conn") do |on|
+            on.message do |channel, message|
+              puts message
+            end
+          end
+
           0
         end
       end
